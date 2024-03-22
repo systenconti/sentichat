41cdea97ad9fe9c519300ff1f072afcfb757ac2c
systenconti <bartoszsowa.bsi@gmail.com>
2024-03-22 23:17:02 +0100

    Make first migration

--- mysite/sentichat/models.py
+++ mysite/sentichat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    content = models.TextField(null=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} sent at {self.timestamp}"
+
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    participants = models.ManyToManyField(User, related_name='chat_rooms')
+    messages = models.ManyToManyField(Message, related_name='chat_room', blank=True)
+
+    def __str__(self):
+        return self.name

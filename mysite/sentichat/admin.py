9cdbe0ca6922f94f85f89be287a143a131060c05
systenconti <bartoszsowa.bsi@gmail.com>
2024-03-22 23:36:24 +0100

    Register first admin models

--- mysite/sentichat/admin.py
+++ mysite/sentichat/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Message, ChatRoom
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("sender", 'timestamp')
+    list_filter = ("sender", "timestamp")
+
+
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_filter = ("participants",)
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(ChatRoom, ChatRoomAdmin)

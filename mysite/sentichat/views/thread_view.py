3bf1c55daa01df93ca2801cb0bd65e9c6d0bbd37
systenconti <bartoszsowa.bsi@gmail.com>
2024-04-21 22:28:32 +0200

    Create thread_view

--- mysite/sentichat/views/thread_view.py
+++ mysite/sentichat/views/thread_view.py
@@ -9,7 +9,7 @@ def thread_view(request, chatroom_id):
     chatroom = get_object_or_404(ChatRoom, id=chatroom_id)
     if request.user not in chatroom.participants.all():
         raise Http404("You do not have permission to view this chatroom.")
-    
+
     messages = chatroom.messages.all().order_by('timestamp')
     context = {'chatroom': chatroom, 'messages': messages}
     return render(request, "thread.html", context=context)

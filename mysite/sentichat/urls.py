c92f799f660f82eb308679244f822d8e0212ee6c
systenconti <bartoszsowa.bsi@gmail.com>
2024-04-21 22:28:02 +0200

    update urls to include thread

--- mysite/sentichat/urls.py
+++ mysite/sentichat/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("main/", views.main_view, name="main"),
     path("", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("main/<int:chatroom_id>/", views.thread_view, name="thread"),
 ]

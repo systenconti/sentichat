daa145fffa3e4900a3d4b0f7833423e19e836913
systenconti <bartoszsowa.bsi@gmail.com>
2024-04-21 23:17:35 +0200

    Add test docs

--- mysite/sentichat/tests/views/test_thread_view.py
+++ mysite/sentichat/tests/views/test_thread_view.py
@@ -5,6 +5,7 @@ from sentichat.models import ChatRoom, Message
 
 
 class ChatRoomTestCase(TestCase):
+    # Setup test objects
     def setUp(self):
         self.user1 = User.objects.create_user(username="user1", password="password123")
         self.user2 = User.objects.create_user(username="user2", password="password123")
@@ -20,10 +21,12 @@ class ChatRoomTestCase(TestCase):
 
         self.client = Client()
 
+    # Test if user gets redirected correctly when not logged in
     def test_redirect_if_not_logged_in(self):
         response = self.client.get(reverse("thread", args=[self.chatroom1.id]))
         self.assertRedirects(response, f"/login/?next=/chatrooms/{self.chatroom1.id}/")
 
+    # Test if user can access his chatroom
     def test_user_can_access_chatroom(self):
         self.client.login(username="user1", password="password123")
         response = self.client.get(reverse("thread", args=[self.chatroom1.id]))
@@ -31,11 +34,13 @@ class ChatRoomTestCase(TestCase):
         self.assertTemplateUsed(response, "thread.html")
         self.assertIn("messages", response.context)
 
+    # Test if user cannot access someone's chatroom
     def test_user_cannot_access_non_participating_chatroom(self):
         self.client.login(username="user1", password="password123")
         response = self.client.get(reverse("thread", args=[self.chatroom2.id]))
         self.assertEqual(response.status_code, 404)
 
+    # Test for non existent chatroom access
     def test_nonexistent_chatroom_access(self):
         self.client.login(username="user1", password="password123")
         response = self.client.get(reverse("thread", args=[999]))

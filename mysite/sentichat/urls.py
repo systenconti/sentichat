9eddef80c445ddcb3bccaa0d0a4fdc3819246c2e
systenconti <bartoszsowa.bsi@gmail.com>
2024-04-21 23:10:59 +0200

    Change urls structure

--- mysite/sentichat/urls.py
+++ mysite/sentichat/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("main/", views.main_view, name="main"),
-    path("", views.login_view, name="login"),
+    path("", views.main_view, name="main"),
+    path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    path("main/<int:chatroom_id>/", views.thread_view, name="thread"),
+    path("chatrooms/<int:chatroom_id>/", views.thread_view, name="thread"),
 ]
